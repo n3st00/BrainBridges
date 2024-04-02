79253fc85a805eb294078245b04b571662fd01a8
n3st00 <119899114+n3st00@users.noreply.github.com>
2024-04-02 14:48:54 +0200

    Added new Project menu + json tests + blank pages

--- JsonFileHelper.cs
+++ JsonFileHelper.cs
@@ -23,7 +23,8 @@ namespace BrainBridges
                 string existingData = await FileIO.ReadTextAsync(file);
 
                 // Deserialize the existing json data into a collection
-                List<object> existingList = JsonConvert.DeserializeObject<List<object>>(existingData) ?? new List<object>();
+                List<object> existingList = JsonConvert.DeserializeObject<List<object>>(existingData) ?? new List<object>(); // create an empty
+                                                                                                                             //collection if it's necessary
                 // add new data to the list
                 existingList.Add(newData);
                 string updatedData = JsonConvert.SerializeObject(existingList);
@@ -41,7 +42,7 @@ namespace BrainBridges
             try
             {
                 StorageFolder localfolder = ApplicationData.Current.LocalFolder;
-                StorageFile file = await localfolder.GetFileAsync(filename);
+                StorageFile file = await localfolder.CreateFileAsync(filename, CreationCollisionOption.OpenIfExists);
                 string jsonData = await FileIO.ReadTextAsync(file);
                 System.Diagnostics.Debug.WriteLine("JSON data read from file:");
                 System.Diagnostics.Debug.WriteLine(jsonData);
@@ -59,7 +60,7 @@ namespace BrainBridges
             try
             {
                 StorageFolder localfolder = ApplicationData.Current.LocalFolder;
-                StorageFile file = await localfolder.GetFileAsync(filename);
+                StorageFile file = await localfolder.CreateFileAsync(filename, CreationCollisionOption.OpenIfExists);
                 string existingData = await FileIO.ReadTextAsync(file);
                 List<object> existingList = JsonConvert.DeserializeObject<List<object>>(existingData) ?? new List<object>();
                 existingList.Add(point);
@@ -76,17 +77,85 @@ namespace BrainBridges
 
         public static async Task ClearJsonFile(string filename)
         {
+            // specify json beginning structure
+            object startJSON = new { ProjectName = filename, ProjectDescription = "" }; //description gets cleared
+                                                                                        // fix?
             try
             {
                 StorageFolder localfolder = ApplicationData.Current.LocalFolder;
-                StorageFile file = await localfolder.GetFileAsync(filename);
-                await FileIO.WriteTextAsync(file, "");
+                StorageFile file = await localfolder.CreateFileAsync(filename, CreationCollisionOption.OpenIfExists);
+                List<object> existingList = new List<object>();
+                existingList.Add(startJSON);
+                string updatedJSON = JsonConvert.SerializeObject(existingList);
+                await FileIO.WriteTextAsync(file, updatedJSON);
                 System.Diagnostics.Debug.WriteLine("Succesfully cleared the JSON file.");
             } catch (Exception ex)
             {
-                System.Diagnostics.Debug.WriteLine($"Error clering JSON file: {ex.Message}");
+                System.Diagnostics.Debug.WriteLine($"Error clearing JSON file: {ex.Message}");
             }
            
+        }
+
+        public static async Task CreateBlankJsonFile(string filename)
+        {
+            try
+            {
+                StorageFolder localfolder = ApplicationData.Current.LocalFolder;
+                StorageFile file = await localfolder.TryGetItemAsync(filename) as StorageFile;
+                if (file == null)
+                {
+                    System.Diagnostics.Debug.WriteLine($"There is no such file as '{filename}'");
+                } else
+                {
+                    await FileIO.WriteTextAsync(file, "");
+                    System.Diagnostics.Debug.WriteLine("Succesfully created a file.");
+                }
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine($"Error creating a JSON file: {ex.Message}");
+            }
+        }
+
+        public static async Task TestCreateNewBlank(string filename)
+        {
+            try
+            {
+                StorageFolder localfolder = ApplicationData.Current.LocalFolder;
+                StorageFile file = await localfolder.TryGetItemAsync(filename) as StorageFile;
+                if (file == null)
+                {
+                    System.Diagnostics.Debug.WriteLine($"There is no such file as '{filename}'");
+                }
+                else
+                {
+                    await FileIO.WriteTextAsync(file, "");
+                    System.Diagnostics.Debug.WriteLine("Succesfully created a file.");
+                }
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine($"Error creating a JSON file: {ex.Message}");
+            }
+        }
+
+        public static async Task TestReadFromJson(string filename)
+        {
+            try
+            {
+                StorageFolder localfolder = ApplicationData.Current.LocalFolder;
+                StorageFile file = await localfolder.CreateFileAsync(filename, CreationCollisionOption.OpenIfExists);
+                string jsonData = await FileIO.ReadTextAsync(file);
+                System.Diagnostics.Debug.WriteLine("JSON data read from file:");
+                System.Diagnostics.Debug.WriteLine(jsonData);
+                await file.DeleteAsync();
+                return;
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine($"Error reading file: {ex.Message}");
+                return;
+            }
         }
     }
 }
--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -6,6 +6,8 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI;
+using Windows.UI.Core;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -21,23 +23,36 @@ namespace BrainBridges
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
+    /// 
+    
+    
     public sealed partial class MainPage : Page
     {
+        TabViewItem existingNewProjectTab = null;
         public MainPage()
         {
             this.InitializeComponent();
         }
 
-        
+        private void CreateNewTabTabView_MP(string header)
+        {
+            var newTab = new TabViewItem { Header = header };
+            TabView_MP.TabItems.Add(newTab);
+        }
         private void TabView_MP_AddTabButtonClick(Microsoft.UI.Xaml.Controls.TabView sender, object args)
         {
-            var newTab = new TabViewItem { Header = "New Tab" };
+            var newTab = new TabViewItem { Header = "Blank Page" };
+            newTab.ContentTemplate = (DataTemplate)Resources["NewPageTemplate"];
             TabView_MP.TabItems.Add(newTab);
         }
 
         private void TabView_MP_TabCloseRequested(Microsoft.UI.Xaml.Controls.TabView sender, Microsoft.UI.Xaml.Controls.TabViewTabCloseRequestedEventArgs args)
         {
             sender.TabItems.Remove(args.Tab);
+            if (args.Tab.Header == "Create New Project")
+            {
+                existingNewProjectTab = null;
+            }
         }
 
        
@@ -60,11 +75,117 @@ namespace BrainBridges
             /*await JsonFileHelper.WriteObjectToJson(fileName, point);
             await JsonFileHelper.ReadFromJsonFile(fileName)*/;
             await JsonFileHelper.ClearJsonFile(fileName);
+            await JsonFileHelper.ReadFromJsonFile(fileName);
+            
         }
 
-        private void MenuFlyoutItem_Click(object sender, RoutedEventArgs e)
+        private void MenuNewItem_Click(object sender, RoutedEventArgs e)
         {
+            foreach (TabViewItem tabItem in TabView_MP.TabItems)
+            {
+                if (tabItem.Header == "Create New Project")
+                {
+                    existingNewProjectTab = tabItem;
+                }
+            }
+            if (existingNewProjectTab == null)
+            {
+                var newTab = new TabViewItem { Header = "Create New Project" };
+                newTab.ContentTemplate = (DataTemplate)Resources["NewFileTemplate"];
+                newTab.IsSelected = true;
+                TabView_MP.TabItems.Add(newTab);
+            } else
+            {
+                existingNewProjectTab.IsSelected = true;
+            }
+           
+        }
 
+        private async void CreateNewProjectButton_Click(object sender, RoutedEventArgs e)
+        {
+            bool can_continue = true;
+            string nametext = null;
+            FrameworkElement button = sender as FrameworkElement;
+
+            if (button != null)
+            {
+                DependencyObject parent = VisualTreeHelper.GetParent(button); 
+                while(parent != null && !(parent is ScrollViewer))
+                {
+                    parent = VisualTreeHelper.GetParent(button); break;
+                }
+                if (parent != null)
+                {
+                    TextBox nameTextBox = FindChildControl<TextBox>(parent, "NameTextBox");
+                    TextBlock charTextBlock = FindChildControl<TextBlock>(parent, "CharactersTextBlock");
+                    if (nameTextBox != null)
+                    {
+                        string text = nameTextBox.Text;
+                        nametext = text;
+                        if (text == "" || text.Contains(" ") || text.Contains("/") || text.Contains(".") || text.Contains("\\") || text.Contains("'") || text.Contains('"') || text.Length > 30)
+                        {
+                            nameTextBox.Focus(FocusState.Programmatic);
+                            charTextBlock.Visibility = Visibility.Visible;
+                            can_continue = false;
+                            System.Diagnostics.Debug.WriteLine("name is wrong");
+                        }
+                        else
+                        {
+                            
+                            System.Diagnostics.Debug.WriteLine("name is right");
+                        }
+                    }
+                    TextBox descrTextBox = FindChildControl<TextBox>(parent, "DescriptionTextBox");
+                    TextBlock lengthTextBlock = FindChildControl<TextBlock>(parent, "LengthTextBlock");
+                    if (descrTextBox != null)
+                    {
+                        string text = descrTextBox.Text;
+                        if (text.Length > 300)
+                        {
+                            descrTextBox.Focus(FocusState.Programmatic);
+                            lengthTextBlock.Visibility = Visibility.Visible;
+                            can_continue = false;
+                            System.Diagnostics.Debug.WriteLine("description is wrong");
+                        }
+                    }
+                }
+                
+            }
+            if (can_continue == true)
+            {
+                await JsonFileHelper.TestCreateNewBlank(nametext);
+                await JsonFileHelper.ClearJsonFile(nametext);
+                await JsonFileHelper.TestReadFromJson(nametext);
+                CreateNewTabTabView_MP(nametext);
+            }
+
+            
+        }
+
+        public static T FindChildControl<T>(DependencyObject control, string ctrlName) where T : FrameworkElement
+        {
+            int childNumber = VisualTreeHelper.GetChildrenCount(control);
+            for (int i = 0; i < childNumber; i++)
+            {
+                DependencyObject child = VisualTreeHelper.GetChild(control, i);
+                FrameworkElement fe = child as FrameworkElement;
+                // Not a framework element or is null
+                if (fe == null) return null;
+
+                if (child is T && fe.Name == ctrlName)
+                {
+                    // Found the control so return
+                    return (T)child;
+                }
+                else
+                {
+                    // Not found it - search children
+                    T subItem = FindChildControl<T>(child, ctrlName);
+                    if (subItem != null)
+                        return subItem;
+                }
+            }
+            return null;
         }
     }
 }
